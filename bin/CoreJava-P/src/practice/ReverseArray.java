90e0edd70613db698d2120c12ffe695cc06cdea4
hall-geeks <hall-geeks@hallgeeks>
2020-07-27 15:11:19 +0530

    First Commit

---
+++ bin/CoreJava-P/src/practice/ReverseArray.java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		String[] names = {"John", "Jammy", "Luke"};
+		System.out.println("original array: " + Arrays.toString(names) );
+		// reversing array with three elements 
+		reverse(names);
+		System.out.println("reversed array: " + Arrays.toString(names) );
+
+	}
+
+	private static void reverse(String[] array) {
+		if(array==null || array.length<2) {
+			return;
+		}
+		for(int i=0;i<array.length/2;i++) {
+			String temp = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = temp;
+
+		}
+	}
+
+}
